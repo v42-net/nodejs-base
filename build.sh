dc42c182b541dcd3975d3bf70aa4205f56134e0d
Peter Hoogendijk <peter@v42.net>
2025-07-05 16:03:42 +0200

    Use local registry

--- build.sh
+++ build.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -e
 cd $(dirname $0)
-IMAGE=ghcr.io/v42-net/$(basename $(pwd)):latest
+
+# IMAGE=ghcr.io/v42-net/$(basename $(pwd)):latest
+IMAGE=registry.v42.net:5443/$(basename $(pwd)):latest
+docker pull node:23-slim
+
 docker build --no-cache --progress=plain -t ${IMAGE} .
 docker image prune -f
 docker push ${IMAGE}
