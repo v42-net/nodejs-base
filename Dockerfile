fc0f80c61a18cb1024dafc0e310f000c66a92056
Peter Hoogendijk <peter@v42.net>
2025-07-05 16:06:54 +0200

    Upgrade to node:24-slim

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
-FROM node:23-slim
+# match "FROM" to pull command in build.sh !!!
+FROM node:24-slim
 ENV NODE_PATH=/usr/local/lib/node_modules NODE_ENV=production
 ENTRYPOINT ["/usr/local/bin/node"]
 CMD ["/opt/app/server.js"]
--- build.sh
+++ build.sh
@@ -2,9 +2,12 @@
 set -e
 cd $(dirname $0)
 
+# match pull to "FROM" in Dockerfile !!!
+docker pull node:24-slim
+
 # IMAGE=ghcr.io/v42-net/$(basename $(pwd)):latest
 IMAGE=registry.v42.net:5443/$(basename $(pwd)):latest
-docker pull node:23-slim
+
 
 docker build --no-cache --progress=plain -t ${IMAGE} .
 docker image prune -f
